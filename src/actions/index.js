858a547426c51542f6b9b1a4a09b0349ad443e39
Ankush Thakur <ankush.thakur53@gmail.com>
2020-08-09 18:05:55 +0530

    write out all action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,10 @@ import {
   SIGN_IN,
   SIGN_OUT,
   CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
 } from './types';
 
 export const signIn = userId => {
@@ -24,5 +28,37 @@ export const createStream = formValues => async dispatch => {
   dispatch({
     type: CREATE_STREAM,
     payload: response.data
+  });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: response.data
+  });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({
+    type: FETCH_STREAM,
+    payload: response.data
+  });
+};
+
+export const editStream = (id, streamDetails) => async dispatch => {
+  const response = streams.put(`/streams/${id}`, streamDetails);
+  dispatch({
+    type: EDIT_STREAM,
+    payload: response.data
+  });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({
+    type: DELETE_STREAM,
+    payload: id
   });
 };
